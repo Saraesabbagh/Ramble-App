ff6b48cb9135c0d4dfc23b594bce23a8295207a7
Paris Monson <parismonson@yahoo.com>
2022-09-01 12:11:28 +0100

    User Schema created with basic tests passing. Also added some extra dependancies

--- react-backend/dist/app.js
+++ react-backend/dist/app.js
@@ -4,8 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const bluebird_1 = __importDefault(require("bluebird"));
 const app = (0, express_1.default)();
 const port = 3000;
+const mongoDbUrl = "mongodb://0.0.0.0/Ramble";
+mongoose_1.default.Promise = bluebird_1.default;
+mongoose_1.default.connect(mongoDbUrl).then(() => { console.log("Database connection worked "); }).catch(err => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
--- react-backend/src/app.ts
+++ react-backend/src/app.ts
@@ -1,6 +1,21 @@
 import express from 'express';
+import mongoose from "mongoose";
+import session from "express-session";
+import bodyParser from "body-parser";
+import path from "path";
+import bluebird from "bluebird";
 const app = express();
 const port = 3000;
+
+const mongoDbUrl =  "mongodb://0.0.0.0/Ramble";
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoDbUrl).then(
+    () => { console.log("Database connection worked ")},
+).catch(err => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
