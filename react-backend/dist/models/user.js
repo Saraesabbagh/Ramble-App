a646136b4657fe897b38df6dd08e161b5ffadbfd
Karolina Joksaite <k.joksaite00@gmail.com>
2022-09-01 14:50:12 +0100

    added pre middleware to hash password

--- react-backend/dist/models/user.js
+++ react-backend/dist/models/user.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const UserSchema = new mongoose_1.default.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
@@ -17,19 +18,47 @@ const UserSchema = new mongoose_1.default.Schema({
     preferences: {
         walk: { type: Boolean, required: true, default: false },
         run: { type: Boolean, required: true, default: false },
-        cycle: { type: Boolean, required: true, default: false }
+        cycle: { type: Boolean, required: true, default: false },
     },
     img: {
         data: Buffer,
         contentType: String,
     },
-    followers: [{
-            userId: { type: mongoose_1.default.Schema.Types.ObjectId, ref: "User" }
-        }],
-    following: [{
-            userId: { type: mongoose_1.default.Schema.Types.ObjectId, ref: "User" }
-        }],
+    followers: [
+        {
+            userId: { type: mongoose_1.default.Schema.Types.ObjectId, ref: 'User' },
+        },
+    ],
+    following: [
+        {
+            userId: { type: mongoose_1.default.Schema.Types.ObjectId, ref: 'User' },
+        },
+    ],
 });
-const User = mongoose_1.default.model("User", UserSchema);
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt_1.default.genSalt(10, function (saltError, salt) {
+            if (saltError) {
+                return next(saltError);
+            }
+            else {
+                bcrypt_1.default.hash(user.password, salt, function (hashError, hash) {
+                    if (hashError) {
+                        return next(hashError);
+                    }
+                    console.log(hash);
+                    user.password = hash;
+                    console.log(user.password);
+                    next();
+                });
+            }
+        });
+    }
+    else {
+        return next();
+    }
+});
+const User = mongoose_1.default.model('User', UserSchema);
 exports.default = User;
 //# sourceMappingURL=user.js.map
