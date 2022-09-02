1e591023071fc97cae7c6bb60259b05e052b0b61
Karolina Joksaite <k.joksaite00@gmail.com>
2022-09-02 16:55:46 +0100

    potential start to auth

--- react-backend/src/controllers/user.ts
+++ react-backend/src/controllers/user.ts
@@ -1,35 +1,63 @@
-import { User } from "../models/user"
-import { body, check, validationResult } from "express-validator";
-import { Request, Response, NextFunction } from "express";
-import { WriteError } from "mongodb";
-import { CallbackError } from "mongoose";
+import { User } from '../models/user';
+import { body, check, validationResult } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+import { WriteError } from 'mongodb';
+import { CallbackError } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 /**
  * Login page.
  * @route POST /api/signup
  */
-export const saveUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const saveUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   // Validation checks
   // If authentication fails
-  console.log(req.body)
+  console.log(req.body);
   const user = new User(req.body);
-  User.findOne({email: req.body.email}, (err, user) => {
-    if (err) {return next(err)}
-    if (user) {
-      res.status(500)
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return next(err);
     }
-
-  })
+    if (user) {
+      res.status(500);
+    }
+  });
   try {
     await user.save();
-    res.status(200)
-    res.json(user)
+    res.status(200);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.redirect('/user/new');
   }
-  catch(err){
-    console.log(err)
-    res.status(500)
-    res.redirect("/user/new")
-  }
-}
+};
 
+export const validateUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
 
+    bcrypt.compare(req.body.password, user.password, function (err, isMatch) {
+      if (err) {
+        return next(err);
+      }
+
+      if (isMatch) {
+        return user;
+        // check with frontend what is best to return
+      } else {
+        res.redirect('/session/new');
+      }
+    });
+  });
+};
