0b579c73dc5044483e5cd587feae16da55894c3d
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-09-05 12:09:59 +0100

    Change button on navbar

--- react-frontend/src/components/atomic-components/Navbar.js
+++ react-frontend/src/components/atomic-components/Navbar.js
@@ -14,14 +14,15 @@ export const Navbar = () => {
 
     return ( 
         <div className="topnav">
-        
-            <a href="/">Home</a>
-            <a href="/mission">About us</a>
-            <a href="/app">App</a>
-        
-            <button onClick={navigateToLogin} className="button"> LOGIN </button>
-            <button onClick={navigateToSignUp} className="button"> SIGN UP </button>
-            
+        <ul>
+            <li> <a href="/">Home</a> </li>
+            <li> <a href="/mission">About us</a> </li>
+            <li> <a href="/app">App</a> </li>
+            <span className="nav-button-span">
+                <li><button onClick={navigateToLogin} className="nav-button"> Login</button></li>
+                <li><button onClick={navigateToSignUp} className="nav-button"> Sign up </button></li>
+            </span>    
+        </ul>    
         </div>
     )
 };
