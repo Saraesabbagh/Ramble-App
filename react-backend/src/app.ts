809b71a67a738aeda7f5073fd80c4e361f828a33
Karolina Joksaite <k.joksaite00@gmail.com>
2022-09-05 13:23:34 +0100

    added config for cookie session

--- react-backend/src/app.ts
+++ react-backend/src/app.ts
@@ -1,65 +1,73 @@
 import express from 'express';
-import mongoose from "mongoose";
-import session from "express-session";
-import bodyParser from "body-parser";
-import path from "path";
-import bluebird from "bluebird";
-import flash from "express-flash"
-import cookieParser from "cookie-parser"
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
+import bluebird from 'bluebird';
+import flash from 'express-flash';
+import cookieSession from 'cookie-session';
+import cors from 'cors';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 // Controller (route Handlers)
 
-import * as userController from "./controllers/user"
+import * as userController from './controllers/user';
 
 const app = express();
 const port = 3001;
+const corsOptions = {
+  origin: 'http://localhost:3001',
+};
 
-app.use(cookieParser());
-app.use(session({
-  key: "user_sid",
-  secret: "super_secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    expires: 1200000,
-  }
-}));
-app.use(flash());
+app.use(
+  cookieSession({
+    name: 'ramble-session',
+    secret: process.env.SESSION_SECRET,
+    httpOnly: true,
+  })
+);
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(session());
-app.use(express.static(path.join(__dirname, "/react-frontend")))
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/react-frontend')));
 
-const mongoDbUrl =  "mongodb://0.0.0.0/Ramble";
+const mongoDbUrl = 'mongodb://0.0.0.0/Ramble';
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoDbUrl).then(
-    () => { console.log("Database connection worked ")},
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    console.log('Database connection worked ');
+  })
+  .catch((err) => {
+    console.log(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
     // process.exit();
-});
+  });
 
 /**
  * API Routes.
  */
 
- app.post("/api/signup", userController.saveUser )
+app.post('/api/signup', userController.saveUser);
 
-
-
- /**
+/**
  * Handles all other routes
  */
 
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "react-frontend/public/index.html"));
+  res.sendFile(
+    path.join(__dirname, '..', '..', 'react-frontend/public/index.html')
+  );
 });
 
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to bezkoder application.' });
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
