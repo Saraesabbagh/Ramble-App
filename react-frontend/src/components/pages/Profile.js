a7dc5f96b96a17b87b4b791a3e18da2f36903238
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-09-06 15:21:19 +0100

    display title when finding by id

--- react-frontend/src/components/pages/Profile.js
+++ react-frontend/src/components/pages/Profile.js
@@ -1,7 +1,31 @@
+import "./HomePage.css";
 import { Page } from "../pages/Page";
+import React, { useEffect, useState } from "react";
 
 export const Profile = () => {
+    const [users, setUser] = useState([]);
+    useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos') //change this for backend url to get the user session
+      .then(response => response.json())
+      .then(json => setUser(json))
+      .catch((err) => {
+        console.log(err.message);
+      })
+  }, [])
+
     return (
-        <Page />
+        
+        <div>
+            <Page />
+            {users.map((user) => {
+                if(user.id === 1){
+                    return (
+                        user.title
+                    )
+                }
+            })}
+                
+           
+        </div>
     )
 }
