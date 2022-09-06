7d3336cc6760958e83435e0e8cba58b06e36af97
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-09-06 12:06:52 +0100

    create a get method to request data from backend

--- react-frontend/src/components/pages/HomePage.js
+++ react-frontend/src/components/pages/HomePage.js
@@ -1,9 +1,21 @@
 import "./HomePage.css";
 import { Page } from "./Page.js";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 export const HomePage = () => {
+  const [journeys, setJourneys] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos') //change this for backend url like '/api/signup'
+      .then(response => response.json())
+      .then(json => setJourneys(json))
+      .catch((err) => {
+        console.log(err.message);
+      })
+  }, [])
+  
+    
    return (
     <div> 
         <Page />
@@ -15,35 +27,35 @@ export const HomePage = () => {
                 <button className="button">Filter</button>
             </div>
         </div>
+
         <div className="homeList">
-        <div className='row'>
+          <div className='row'>
             <div className='column'>
               <div className='map-column'>
-
-              <img className="cyclegroup" src={require("../images/samplemap.png")} alt="map" width="250" />
-             
-
+                <img className="cyclegroup" src={require("../images/samplemap.png")} alt="map" width="250" />
               </div>
             </div>
-            <div className='column'>
-              <div className='text-column'>
 
-              <h3>Walk with kate</h3>
-                <p>Date and Time: </p>
-                <p>Location: </p>
-                <p>Duration: </p>
-                <p>Find out more about this Journey
-                <p><a className="button" href="/journey/id">here</a></p>
-                </p>
-
-                
-              </div>
+          <div className='column'>
+            <div className='text-column'>
+            {/* generate all the routes in the database */}
+            {/* add a button with request to join */}
+            {journeys.map((journey) => {
+              return (
+                <div>
+                  <h3>Walk with kate{journey.id}</h3>
+                  {/* <p>Date and Time: {journey.created_at}</p>
+                  <p>Location:{journey.user.handle} </p>
+                  <p>Duration: {journey.likes.length}</p> */}
+                  <p>Find out more about this Journey</p>
+                  <p><a className="button" href="/journey/id">here</a></p>
+                </div>
+              )
+              })}
             </div>
           </div>
         </div>
-    
-
+      </div>
     </div>
    )
-   
 }
