0ec8a873d2a27e6587936d902829a82c700a65fb
Paris Monson <parismonson@yahoo.com>
2022-09-06 10:23:09 +0100

    Started map_api

--- react-backend/src/controllers/map_box_api.ts
+++ react-backend/src/controllers/map_box_api.ts
@@ -1,15 +1,33 @@
 import { Request, Response, NextFunction } from "express";
 
-
 /**
- * Login page.
+ *
  * @route POST /api/route
  */
 
-export const getRoute = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const getRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const start_coordinates = req.body.start_coordinates;
+  const end_coordinates = req.body.end_coordinates;
+};
 
-  const start_coordinates = req.body.start_coordinates
-  const end_coordinates = req.body.end_coordinates
+/**
+ *
+ * @route POST /api/static
+ */
 
-}
-)
+export const getStaticMap = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const URL = "Test";
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error("Error: ", err));
+};
