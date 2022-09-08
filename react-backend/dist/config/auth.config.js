b41244fc995ddc9d51fbb6b404ab7a9d6cde64ea
Tayanne Bencardino <100074381+taybenca@users.noreply.github.com>
2022-09-08 15:21:02 +0100

    Revert "Newjoin"

--- react-backend/dist/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    secret: 'soup-and-cats',
-};
-//# sourceMappingURL=auth.config.js.map
--- react-frontend/src/components/pages/HomePage.js
+++ react-frontend/src/components/pages/HomePage.js
@@ -4,139 +4,94 @@ import { Page } from "./Page.js";
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
+
 export const HomePage = (props) => {
   const [journeys, setJourneys] = useState([]);
   console.log(props.user);
-
+  
   // Ask information to back-end to get all the journeys
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/") //https://jsonplaceholder.typicode.com/todos/ ///api/all_routes
-      .then((response) => response.json())
-      .then((json) => setJourneys(json))
+    fetch('https://jsonplaceholder.typicode.com/todos/') //https://jsonplaceholder.typicode.com/todos/ ///api/all_routes
+      .then(response => response.json())
+      .then(json => setJourneys(json))
       .catch((err) => {
         console.log(err.message);
-      });
-  }, []);
-
+      })
+  }, [])
+  
   // Navigate to a page to create new journey
   const navigate = useNavigate();
 
   const navigateToNewJourney = () => {
-    navigate("/journey/new");
-  };
+    
+    navigate('/journey/new')
+  }
+    
+  // Data for dropdown list to do the filter button
 
-  //Add information about participants into the database
-  // const whenSubmit = (event) => {
-  //   event.preventDefault();
-  //   // const userId = props.user._id
-  //   // const journeyId = journey._id
+  const disciplines = [
+    "Walking",
+    "Running",
+    "Cycling"
+]
 
-  // fetch("/api/addParticipant", { // add url to post participants into database
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({userId: userId.value, journeyId: journeyId.value}) //send journey_id and partipant_id
-  //   })
-  //   .then((response) => (response.json()))
-  //   .then((data) => {
-  //     console.log(data);
-  //     if(data.message === "You joined") {
-  //       window.alert("You joined!");
-  //       navigate('/home')
-  //     } else {
-  //     window.alert(data.message)
-  //   }} )
-  //   .catch((error) => {
-  //     console.error("Error", error);
-  //   });
-  // };
-  return (
-    <div>
-      <Page />
-
-      <div className="homeHero">
-        <div className="homeHeroTextBox">
-          <h1>Hi {props.user.firstName}!</h1>
-          <h2>What would you like to do today?</h2>
-          <button onClick={navigateToNewJourney} className="button">
-            ADD NEW JOURNEY
-          </button>
+   return (
+    <div> 
+        <Page />
+      
+        <div className="homeHero"> 
+            <div className="homeHeroTextBox">
+                <h1>Hi {props.user.firstName}!</h1>
+                <h2>What would you like to do today?</h2>
+                <button onClick={navigateToNewJourney} className="button">ADD NEW JOURNEY</button>
+                <DropDownList name="discipline" items={disciplines}/>
+            </div>
+            
         </div>
-      </div>
 
       {journeys.map((journey) => {
+        const sendDataToProfile = () => {
+          navigate('/profile', {state:{id:1, name:journey.host_id}});
+        }
+        // const currentUserId = "Hello, I'm user id" //journey.host_id
+        // const currentUserId = journey.host_id
         return (
-          <div className="homeList">
-            <div className="row">
-              {/* image card */}
-              <div className="column">
-                <div className="map-column">
-                  <img
-                    className="cyclegroup"
-                    src={require("../images/samplemap.png")}
-                    alt="map"
-                    width="250"
-                  />
-                </div>
-              </div>
-
-              {/* description and button card */}
-              <div className="column">
-                <div className="map-column">
-                  <button
-                    onClick={(event) => {
-                      event.preventDefault();
-                      const userId = props.user._id;
-
-                      fetch("/api/addParticipant", {
-                        // add url to post participants into database
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                          userId: userId,
-                          journeyId: journey._id,
-                        }), //send journey_id and partipant_id
-                      })
-                        .then((response) => response.json())
-                        .then((data) => {
-                          console.log(data);
-                          if (data.message === "Participant added!") {
-                            window.alert("You joined!");
-                            navigate("/home");
-                          } else {
-                            window.alert(data.message);
-                          }
-                        })
-                        .catch((error) => {
-                          console.error("Error", error);
-                        });
-                    }}
-                    className="button"
-                  >
-                    Join now!
-                  </button>
-                  <p>{journey.description}</p>
-                </div>
-              </div>
-
-              {/* text card */}
-              <div className="column">
-                <div className="text-column">
-                  <h3>{journey.title}</h3>
-                  <p>{journey.discipline}</p>
-                  <p>Date and Time: {journey.startTime}</p>
-                  <p>Start location:{journey.startPoint} </p>
-                  <p>End location: {journey.endPoint}</p>
-                  <p>Number of participants: {journey.participants.length}</p>
-                </div>
+        <div className="homeList">
+          
+          <div className='row'>
+            {/* image card */}
+            <div className='column'>
+              <div className='map-column'>
+                <img className="cyclegroup" src={require("../images/samplemap.png")} alt="map" width="250" />
               </div>
             </div>
+
+            {/* description and button card */}
+              <div className='column'>
+                <div className='map-column'>
+                <button className="button" href="/journey/id">Join now!</button>
+                <p>{journey.description}</p>
+                </div>
+              </div>
+
+            {/* text card */}
+                  <div className='column'>
+                    <div className= 'text-column'>
+                      <h3>{journey.title}</h3>
+                      <p>{journey.discipline}</p>
+                      <p>Date and Time: {journey.startTime}</p>
+                      <p>Start location:{journey.startPoint} </p>
+                      <p>End location: {journey.endPoint}</p>
+                      <button onClick={() => {sendDataToProfile()}}>Host</button>
+                      <button className="button" href="/journey/id">Find out more</button>
+                    </div>
+            </div>
           </div>
-        );
-      })}
-    </div>
-  );
-};
+        </div>
+        
+            )})}
+
+            </div>
+            
+      )}
+      
