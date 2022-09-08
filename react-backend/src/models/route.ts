28a6d905789294d8597086a1d5077945427df59b
Paris Monson <parismonson@yahoo.com>
2022-09-08 14:05:31 +0100

    No time

--- react-backend/src/models/route.ts
+++ react-backend/src/models/route.ts
@@ -5,6 +5,7 @@ const RouteSchema = new mongoose.Schema({
   discipline: { type: String, required: true },
   duration: { type: String, required: true },
   distance: { type: String, required: true},
+  date: { type: String, required: true},
   title: { type: String, required: true },
   description: { type: String, required: true },
   startTime: { type: String, required: true },
