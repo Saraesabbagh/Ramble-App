19d33393b70738575b52e7ecedc9783a5bded983
Paris Monson <parismonson@yahoo.com>
2022-09-09 16:09:37 +0100

    Final Commit

--- react-frontend/src/components/atomic-components/journeyRender.js
+++ react-frontend/src/components/atomic-components/journeyRender.js
@@ -1,18 +1,19 @@
 import React from "react";
-
+ import "./journeyRender.css";
 
 export const JourneyRender = (props) => {
     
     return ( 
         <div className="journey-container">
-            <p>{props.journey.host_id}</p>
-            <p>{props.journey.title}</p>
-            <p>{props.journey.description}</p>
-            <p>{props.journey.discipline}</p>
-            <p>{props.journey.startTime}</p>
-            <p>{props.journey.endPoint}</p>
-            <p>{props.journey.duration} Minutes</p>
-            <p>{props.journey.distance} Miles </p>
+            <h1>{props.journey.title}</h1>
+            <p>Description: {props.journey.description}</p>
+            <p>Excercise Type: {props.journey.discipline}</p>
+            <p>Start at: {props.journey.startPoint}</p>
+            <p>Finish at: {props.journey.endPoint}</p>
+            <p>Duration: {props.journey.duration} Minutes</p>
+            <p>Distance: {props.journey.distance} Miles </p>
+            <p>Date: {props.journey.date}</p>
+            <p>Time: {props.journey.startTime}</p>
             <img src={props.journey.img} alt="Journey Map"/>
         </div>
     )
--- react-frontend/src/components/pages/HomePage.js
+++ react-frontend/src/components/pages/HomePage.js
@@ -1,8 +1,7 @@
 import "./HomePage.css";
-import { DropDownList } from "../atomic-components/DropDownList";
 import { Page } from "./Page.js";
 import React, { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 export const HomePage = (props) => {
@@ -11,7 +10,7 @@ export const HomePage = (props) => {
   
   // Ask information to back-end to get all the journeys
   useEffect(() => {
-    fetch('/api/all_routes') //https://jsonplaceholder.typicode.com/todos/ ///api/all_routes
+    fetch('/api/all_routes') 
       .then(response => response.json())
       .then(json => setJourneys(json))
       .catch((err) => {
@@ -27,13 +26,6 @@ export const HomePage = (props) => {
     navigate('/journey/new')
   }
     
-  // Data for dropdown list to do the filter button
-
-  const disciplines = [
-    "Walking",
-    "Running",
-    "Cycling"
-]
 
    return (
     <div> 
@@ -44,17 +36,12 @@ export const HomePage = (props) => {
                 <h1>Hi {props.user.firstName}!</h1>
                 <h2>What would you like to do today?</h2>
                 <button onClick={navigateToNewJourney} className="button">ADD NEW JOURNEY</button>
-                <DropDownList name="discipline" items={disciplines}/>
             </div>
             
         </div>
 
       {journeys.map((journey) => {
-        const sendDataToProfile = () => {
-          navigate('/profile', {state:{id:1, name:journey.host_id}});
-        }
-        // const currentUserId = "Hello, I'm user id" //journey.host_id
-        // const currentUserId = journey.host_id
+        
         return (
         <div className="homeList">
           
@@ -62,32 +49,60 @@ export const HomePage = (props) => {
             {/* image card */}
             <div className='column'>
               <div className='map-column'>
-                <img className="cyclegroup" src={journey.img} alt="map" width="250" />
+                <img className="cyclegroup" src={journey.img} alt="map" width="500" />
               </div>
+              <div className='map-column'>
+                <button className="button" onClick={(event) => {
+                    event.preventDefault();
+
+                    const userId = props.user._id;
+
+                    console.log('USER ID: ', userId);
+                    console.log('JOURNEY ID: ', journey._id);
+                    fetch('/api/addParticipant', {
+                      // add url to post participants into database
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({
+                        userId: userId,
+                        journeyId: journey._id,
+                      }), //send journey_id and partipant_id
+                    })
+                      .then((response) => response.json())
+                      .then((data) => {
+                        console.log(data);
+                        if (data.message === 'Participant added!') {
+                          window.alert('You joined!');
+                          navigate('/home');
+                        } else {
+                          window.alert(data.message);
+                        }
+                      })
+                      .catch((error) => {
+                        console.error('Error', error);
+                      });
+                  }}>Join now!</button>
+                <p>Description:</p>
+                <p>{journey.description}</p>
+                </div>
             </div>
 
             {/* description and button card */}
-              <div className='column'>
-                <div className='map-column'>
-                <button className="button" href="/journey/id">Join now!</button>
-                <p>{journey.description}</p>
-                </div>
-              </div>
+              
 
             {/* text card */}
                   <div className='column'>
                     <div className= 'text-column'>
-                      <h3>{journey.title}</h3>
-                      <p>{journey.discipline}</p>
+                      <h2>{journey.title}</h2>
+                      <p>Excercise Type: {journey.discipline}</p>
                       <p>Date: {journey.date}</p>
                       <p>Time: {journey.startTime}</p>
                       <p>Start location:{journey.startPoint} </p>
                       <p>End location: {journey.endPoint}</p>
                       <p>Distance: {journey.distance} Miles</p>
                       <p>Duration: {journey.duration} Minutes</p>
-
-                      <button onClick={() => {sendDataToProfile()}}>Host</button>
-                      <button className="button" href="/journey/id">Find out more</button>
                     </div>
             </div>
           </div>
